f0b793e9cd51688188e7acd569083fadbb675617
Ahmad Imran <ahmadimran2728@gmail.com>
2025-12-13 11:36:19 +0500

    Fix product routing

--- backend/src/routes/product.routes.js
+++ backend/src/routes/product.routes.js
@@ -1,10 +1,10 @@
 import { Router } from "express";
-import * as controller from '../controllers/product.controller.js'
+import * as controller from "../controllers/product.controller.js";
 
 const router = Router();
 
-router.get('/', controller.getAll);
-router.post('/:id', controller.create);
-router.put('/:id', controller.update);
+router.get("/", controller.getAll);
+router.post("/", controller.create);
+router.put("/:id", controller.update);
 
 export default router;
