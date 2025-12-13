b5e190513e18189b9c9b5d36bd39ced8e262b09a
Ahmad Imran <ahmadimran2728@gmail.com>
2025-12-13 11:34:49 +0500

    Add product controller

---
+++ backend/src/controllers/product.controller.js
@@ -0,0 +1,20 @@
+import * as service from "../services/product.service.js";
+
+export const getAll = async (req, res) => {
+    const getAll = await service.getAll(req.user.id);
+
+    return res
+        .status(200)
+        .json({ message: "Getting all products", data: getAll });
+};
+
+export const create = async (req, res) => {
+    const newProduct = await service.create(req.body);
+
+    return res
+        .status(200)
+        .json({ message: "Product created", data: newProduct });
+};
+
+export const update = async (req, res) =>
+    res.json(await service.update(req.params.id, req.body));
