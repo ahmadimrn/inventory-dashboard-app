4b718c6a2c31b0e163e9c4439bcdb86b441bed4d
Ahmad Imran <ahmadimran2728@gmail.com>
2025-12-13 11:40:57 +0500

    Add urlencoded middleware in app.js

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,10 +1,18 @@
-import express from 'express';
-import cors from 'cors';
-import 'dotenv/config'
+import express from "express";
+import cors from "cors";
+import "dotenv/config";
+import indexRoutes from "./routes/index.routes.js";
 
 const app = express();
 
+// middlewares
+
 app.use(cors());
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// routing
+
+app.use("/api", indexRoutes);
 
 export default app;
