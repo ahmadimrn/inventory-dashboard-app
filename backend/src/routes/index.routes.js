39386280c8837ae5cf44627f3e15c0dd808a2b89
Ahmad Imran <ahmadimran2728@gmail.com>
2025-12-14 22:03:08 +0500

    Add authMiddleware in index.routes

--- backend/src/routes/index.routes.js
+++ backend/src/routes/index.routes.js
@@ -3,11 +3,12 @@ import productRoutes from "./product.routes.js";
 import importRoutes from "./import.routes.js";
 import userRoutes from "./user.routes.js";
 import authRoutes from "./auth.routes.js";
+import { authMiddleware } from "../middleware/auth.middleware.js";
 
 const router = Router();
-router.use("/products", productRoutes);
-router.use("/import", importRoutes);
-router.use("/user", userRoutes);
+router.use("/products", authMiddleware, productRoutes);
+router.use("/import", authMiddleware, importRoutes);
+router.use("/user", authMiddleware, userRoutes);
 router.use("/auth", authRoutes);
 
 export default router;
