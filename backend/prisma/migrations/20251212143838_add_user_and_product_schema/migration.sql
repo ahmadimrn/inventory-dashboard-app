7a9436e471b16eceff9aa10470cedf4a7a79613d
Ahmad Imran <ahmadimran2728@gmail.com>
2025-12-14 22:52:35 +0500

    Add prisma migration

--- backend/prisma/migrations/20251212143838_add_user_and_product_schema/migration.sql
+++ backend/prisma/migrations/20251212143838_add_user_and_product_schema/migration.sql
@@ -1,7 +1,9 @@
 -- CreateTable
 CREATE TABLE "User" (
     "id" SERIAL NOT NULL,
+    "email" TEXT NOT NULL,
     "name" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
 
     CONSTRAINT "User_pkey" PRIMARY KEY ("id")
 );
@@ -9,6 +11,7 @@ CREATE TABLE "User" (
 -- CreateTable
 CREATE TABLE "Product" (
     "id" SERIAL NOT NULL,
+    "user_id" INTEGER NOT NULL,
     "name" TEXT NOT NULL,
     "price" INTEGER NOT NULL,
     "stock" INTEGER NOT NULL,
@@ -16,3 +19,9 @@ CREATE TABLE "Product" (
 
     CONSTRAINT "Product_pkey" PRIMARY KEY ("id")
 );
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
+
+-- AddForeignKey
+ALTER TABLE "Product" ADD CONSTRAINT "Product_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
