d786f638fe9140fda23794114ff2f1fc2a6e28d8
Ahmad Imran <ahmadimran2728@gmail.com>
2025-12-14 19:57:04 +0500

    Add auth routes in index.routes

--- backend/src/routes/index.routes.js
+++ backend/src/routes/index.routes.js
@@ -2,10 +2,12 @@ import { Router } from "express";
 import productRoutes from "./product.routes.js";
 import importRoutes from "./import.routes.js";
 import userRoutes from "./user.routes.js";
+import authRoutes from "./auth.routes.js";
 
 const router = Router();
 router.use("/products", productRoutes);
 router.use("/import", importRoutes);
 router.use("/user", userRoutes);
+router.use("/auth", authRoutes);
 
 export default router;
