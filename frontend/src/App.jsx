e3ed8ccc604eb78fd5c4d4ee8037db41e54ee6ae
Ahmad Imran <ahmadimran2728@gmail.com>
2025-12-28 19:48:23 +0500

    Refactor into app providers and app routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,53 +1,10 @@
-import "./App.css";
-import { Route } from "react-router";
-import { Routes } from "react-router";
-import Products from "./pages/Products";
-import Analytics from "./pages/Analytics";
-import MainLayout from "./pages/MainLayout";
-import AuthLayout from "./pages/AuthLayout";
-import Login from "./pages/Login";
-import Register from "./pages/Register";
-import User from "./pages/User";
-import { Navigate, Outlet } from "react-router";
-import "@fontsource/poppins/300.css";
-import "@fontsource/poppins/400.css";
-import "@fontsource/poppins/500.css";
-import "@fontsource/poppins/700.css";
-import { ThemeProvider } from "@mui/material";
-import theme from "./theme";
+import AppProviders from "./AppProviders";
+import AppRoutes from "./routes/AppRoutes";
 
-const App = () => {
-    const RequireAuth = () => {
-        // const token = localStorage.getItem("token")
-        const token = true; // TEMPORARILY
-        return token ? <Outlet /> : <Navigate to="/login" replace />;
-    };
-
-    return (
-        <div className="App">
-            <ThemeProvider theme={theme}>
-                <Routes>
-                    <Route element={<AuthLayout />}>
-                        <Route path="/login" element={<Login />} />
-                        <Route path="/register" element={<Register />} />
-                    </Route>
-
-                    <Route element={<RequireAuth />}>
-                        <Route element={<MainLayout />}>
-                            <Route path="/products" element={<Products />} />
-                            <Route path="/analytics" element={<Analytics />} />
-                            <Route path="/user" element={<User />} />
-                        </Route>
-                    </Route>
-
-                    <Route
-                        path="/"
-                        element={<Navigate to="/products" replace />}
-                    />
-                </Routes>
-            </ThemeProvider>
-        </div>
-    );
-};
+const App = () => (
+    <AppProviders>
+        <AppRoutes />
+    </AppProviders>
+);
 
 export default App;
