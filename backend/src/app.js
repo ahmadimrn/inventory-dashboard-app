19f09c6cee4633d5b010debe231cd74249296704
Ahmad Imran <ahmadimran2728@gmail.com>
2025-12-29 22:58:09 +0500

    Add cookie parser middleware and add cors origin for frontend

--- backend/src/app.js
+++ backend/src/app.js
@@ -3,18 +3,27 @@ import cors from "cors";
 import "dotenv/config";
 import indexRoutes from "./routes/index.routes.js";
 import errorHandler from "./middleware/errorHandler.middleware.js";
+import cookieParser from "cookie-parser";
 
 const app = express();
 
-// middlewares
+// global middlewares
 
-app.use(cors());
+app.use(
+    cors({
+        origin: "http://localhost:5173",
+        credentials: true,
+    })
+);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
 
 // routing
 
 app.use("/api", indexRoutes);
+
+// error handler
 
 app.use(errorHandler);
 
