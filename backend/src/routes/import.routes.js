cc7724d91c2d2f4ef33c427e245002b12165921f
Ahmad Imran <ahmadimran2728@gmail.com>
2026-01-01 21:38:14 +0500

    Delete import routes

--- backend/src/routes/import.routes.js
+++
@@ -1,8 +0,0 @@
-import { Router } from 'express';
-import * as controller from '../controllers/import.controller.js';
-
-const router = Router();
-
-router.post('/', controller.importData);
-
-export default router;
