ff6b0e6a5e780d1ab1c7791a2dec2f0aea3d710b
Ahmad Imran <ahmadimran2728@gmail.com>
2026-01-03 14:43:12 +0500

    Add analytics routes in index routes

--- backend/src/routes/index.routes.js
+++ backend/src/routes/index.routes.js
@@ -1,14 +1,12 @@
 import { Router } from "express";
 import productRoutes from "./product.routes.js";
-import importRoutes from "./import.routes.js";
-import userRoutes from "./user.routes.js";
 import authRoutes from "./auth.routes.js";
+import analyticsRoutes from "./analytics.routes.js";
 import { authMiddleware } from "../middleware/auth.middleware.js";
 
 const router = Router();
 router.use("/products", authMiddleware, productRoutes);
-router.use("/import", authMiddleware, importRoutes);
-router.use("/user", authMiddleware, userRoutes);
+router.use("/analytics", authMiddleware, analyticsRoutes);
 router.use("/auth", authRoutes);
 
 export default router;
