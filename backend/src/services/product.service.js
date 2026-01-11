e92df1d95d2536d51f5025c081fbd9cc4d87d1a6
Ahmad Imran <ahmadimran2728@gmail.com>
2026-01-11 15:25:26 +0500

    Add product deletion function in product service

--- backend/src/services/product.service.js
+++ backend/src/services/product.service.js
@@ -1,46 +1,64 @@
 import { prisma } from "../config/db.config.js";
 
-export const getAll = async (user_id) => {
-    const getAll = await prisma.product.findMany({
-        where: {
-            user_id: user_id,
-        },
+export const getAll = async (userId) => {
+    const products = await prisma.product.findMany({
+        where: { userId },
     });
 
-    return getAll;
+    return products;
 };
 
 export const create = async (reqBody, userId) => {
-    const { name, price, stock, description } = reqBody;
+    const { name, price, stock, category } = reqBody;
 
     const newProduct = await prisma.product.create({
         data: {
-            user_id: userId,
+            userId,
             name,
             price,
             stock,
-            description,
+            category,
         },
     });
 
     return newProduct;
 };
 
-export const update = async (productId, reqBody, userId) => {
-    const { name, price, stock, description } = reqBody;
+export const update = async (productId, userId, data) => {
+    const { name, price, stock, category } = data;
 
     const updatedProduct = prisma.product.update({
         where: {
             id: productId,
-            user_id: userId,
+            userId: userId,
         },
         data: {
             name,
             price,
             stock,
             description,
+            category,
         },
     });
 
     return updatedProduct;
+};
+
+export const deleteProduct = async (productId, userId) => {
+    const product = await prisma.product.findFirst({
+        where: {
+            id: productId,
+            userId: userId,
+        },
+    });
+
+    if (!product) {
+        throw new Error("Product not found or unauthorized");
+    }
+
+    await prisma.product.delete({
+        where: { id: productId },
+    });
+
+    return;
 };
