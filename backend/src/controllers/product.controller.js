b8efc2ca2d0347f9bd930b889762449bb7fedc0a
Ahmad Imran <ahmadimran2728@gmail.com>
2026-01-11 15:25:59 +0500

    Add delete product controller

--- backend/src/controllers/product.controller.js
+++ backend/src/controllers/product.controller.js
@@ -27,12 +27,29 @@ export const create = async (req, res, next) => {
 
 export const update = async (req, res, next) => {
     try {
-        const updatedProduct = await service.update(req.params.id, req.body);
+        const productId = req.params.id;
+        const userId = req.user.id;
+        const data = req.body;
+
+        const updatedProduct = await service.update(productId, userId, data);
 
         return res.status(200).json({
             message: "Product updated successfully",
             data: updatedProduct,
         });
+    } catch (error) {
+        next(error);
+    }
+};
+
+export const deleteProduct = async (req, res, next) => {
+    try {
+        const id = parseInt(req.params.id);
+        const userId = req.user.id;
+
+        await service.deleteProduct(id, userId);
+
+        return res.status(200).json({ message: "Deleted item succesfully" });
     } catch (error) {
         next(error);
     }
